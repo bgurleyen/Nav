aacdeed21ee6dbe04ac9923122116702e05da6da
bgurleyen <bgurleyen@hotmail.com>
2021-09-29 02:46:00 +0300

    other rts data structure

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -68,16 +68,16 @@ public class Move : Singleton<Move>
             TempPtsPos[j] = Pos;
         } 
         
-        for (int j = 1; j < 3; j++)                                                      //Locate Virtual points on EditMap
+        for (int j = 1; j < 20; j++)                                                      //Locate Virtual points on EditMap
         {
         
              pt = GameObject.Find("pt (" + (j + 50) + ")");
-            VirtualPtsPos[j].x = Pos.x+ virtualPoints[Level].VirtualPointsItems[j-1].x;
-             VirtualPtsPos[j].y = Pos.y+virtualPoints[Level].VirtualPointsItems[j-1].y;
+            VirtualPtsPos[j].x = Pos.x+ virtualPoints[Level].VirtualPointsItems[j-1].x+29.3385f;
+             VirtualPtsPos[j].y = Pos.y+virtualPoints[Level].VirtualPointsItems[j-1].y-6.562393f;
             pt.transform.localPosition =  VirtualPtsPos[j];
         }
 
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 11; j++)
         {
             GameObject A = GameObject.Find("AC (" + (j) + ")");                            // Init Other ACs 
         }
@@ -92,6 +92,11 @@ public class Move : Singleton<Move>
         StartCoroutine(MoveAC(1));
         StartCoroutine(MoveAC(2));
         StartCoroutine(MoveAC(3));
+        StartCoroutine(MoveAC(4));
+        StartCoroutine(MoveAC(5));
+        //StartCoroutine(MoveAC(6));
+
+
         StartCoroutine(MoveMyAC());
 
     }
