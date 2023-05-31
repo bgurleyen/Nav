0345e07d47733728cbac07ee3adb0135ac987bc8
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2023-05-31 16:51:54 +0200

    WEbGl Build

--- Assets/_Game/Parts/MCP/ToggleButton3DLinker.cs
+++ Assets/_Game/Parts/MCP/ToggleButton3DLinker.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -12,7 +8,6 @@ public class ToggleButton3DLinker : MonoBehaviour, IPointerUpHandler, IPointerDo
     [SerializeField] private Renderer indicatorMesh;
     [SerializeField] private Material onMat;
     [SerializeField] private Material offMat;
-    [SerializeField] private AnimatorController meshAnimatorController;
 
     private Material[] _materials;
     private Animator _meshAnimator;
@@ -23,12 +18,6 @@ public class ToggleButton3DLinker : MonoBehaviour, IPointerUpHandler, IPointerDo
         var toggle = GetComponent<Toggle>();
         toggle.onValueChanged.AddListener(SetState);
         _meshAnimator = indicatorMesh.gameObject.GetComponent<Animator>();
-        if (_meshAnimator == null)
-        {
-            _meshAnimator = indicatorMesh.gameObject.AddComponent<Animator>();
-            _meshAnimator.runtimeAnimatorController = meshAnimatorController;
-        }
-        
     }
 
     private void SetState(bool on)
